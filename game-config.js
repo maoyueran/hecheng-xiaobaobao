275ef3ce805ad0e245a26763b94ac8dd654abcf7
maoyueran <3476282710@qq.com>
2026-08-26 21:33:06 +0800

    deploy game-config.js

---
+++ game-config.js
@@ -0,0 +1,29 @@
+(function () {
+  "use strict";
+
+  window.MERGE_GAME_CONFIG = {
+    id: "baby-demo",
+    assetBase: "images/",
+    spawnLevelCount: 5,
+
+    ui: {
+      title: "合成小宝宝",
+      description: "合成小宝宝"
+    },
+
+    levels: [
+      { radius: 13, image: "01.png" },
+      { radius: 16, image: "02.png" },
+      { radius: 20, image: "03.png" },
+      { radius: 25, image: "04.png" },
+      { radius: 31, image: "05.png" },
+      { radius: 39, image: "06.png" },
+      { radius: 49, image: "07.png" },
+      { radius: 61, image: "08.png" },
+      { radius: 75, image: "09.png" },
+      { radius: 91, image: "10.png" },
+      { radius: 109, image: "11.png" },
+      { radius: 129, image: "12.png" }
+    ]
+  };
+}());
